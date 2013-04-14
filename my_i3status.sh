ff73cf660f9fbaf59aae05239fcf056a8e3041bb
mrnda <mrnda@lipstick.(none)>
2013-04-15 00:04:40 +0200

    changes

--- my_i3status.sh
+++ my_i3status.sh
@@ -1,15 +1,16 @@
 #!/bin/zsh
 # shell script to append a ⚡ to i3status when charging
 
-i3status | (read line && echo $line && read line && echo $line && while :
+i3status -c $HOME/.i3/i3status.conf | (read line && echo $line && read line && echo $line && while :
 do
 		if [[ $(acpi | grep -i -c "Discharging") != "1" ]]; then
-			charge=',{"name":"charge","full_text":"⚡"}]'
+			charge=',{"name":"charge","full_text":"⚡"}'
 		else
-			charge=']'
+			charge=''
 		fi
-
-        read line
-        line=$(echo $line | tr -d ])
-        echo -n "$line $charge" || exit 1
+		read line
+		line=$(echo $line | tr -d ])
+		echo -n "$line $charge]" || exit 1
+		# read line && echo $line
 done)
+
